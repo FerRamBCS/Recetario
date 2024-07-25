5d4f9b0da17b5416f48a7b0ded6be30ee634840d
FerRamBCS <jfer1302@gmail.com>
2024-07-24 23:40:57 -0700

    SAVE

--- app/src/main/java/com/example/recetario/MainActivity.kt
+++ app/src/main/java/com/example/recetario/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     mealId?.let { MealDetailScreen(it) }
 
 
-                   // MenuScreen(recetarioViewModel = viewModel)
+                   // MenuScreen(recetarioViewModel = viewModel) 
                 }
             }
         }
